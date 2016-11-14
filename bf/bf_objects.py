721ba91764c9e41c11f21931b0c3db2d82c691ab
SpiveeWorks <spiveehere@gmail.com>
2016-11-14 14:31:11 +1100

    split files and create bfo
    bfo represents some bf code in a way that allows for encapsulation, and naming of locations in the tape.
    there is meant to be a one to one correspondance between bf code and bfo, with the exception of redundancies such as '<>' and '><'

---
+++ bf/bf_objects.py
@@ -0,0 +1,71 @@
+
+# gives a string with enough < or > to apply a required shift
+def bf_shift_str(diff):
+    if diff > 0:
+        shift_char = '>'
+    else:
+        shift_char = '<'
+        diff *= -1
+    return ''.join(shift_char for i in range(diff))
+
+class bf_shifter:
+    def __init__(self, current_loc=0):
+        self.loc = current_loc
+    
+    def shift(self, new_loc, silent_shift=0):
+        diff = new_loc - self.loc
+        self.loc = new_loc
+        return bf_shift_str(diff + silent_shift)
+
+
+#bfo_args()[x] gives x, whereas bfo_args(non_zero_list)[x] gives non_zero_list[x]
+#bfo_args() is meant to represent bf routines that operate on the tape directly
+class bfo_args(list):
+    def __getitem__(self, ind):
+        if self:
+            return list.__getitem__(self, ind)
+        else:
+            return ind
+
+
+class bfo_primitive:
+    def __init__(self, bf):
+        self.bf = bf
+    
+    def do(self, shifter, args):
+        ret = ''
+        try:
+            for arg in args:
+                ret += shifter.shift(arg)
+                ret += self.bf
+        except(TypeError):
+            ret += shifter.shift(args)
+            ret += self.bf
+
+class bfo:
+    def __init__(self, routine):
+        self.routine = routine  # sequence of length 2 sequences, similar to [(obj, [callargs])]
+                                #    representing instruction: obj.do(shift, callargs)
+        
+    def do(self, shifter, argmap):
+        ret = ''
+        for obj, subargs in routine:
+            subargs = bfo_args(argmap[subarg] for subarg in subargs)
+            ret += obj.do(shifter, subargs)
+
+class bfo_loop(bfo):
+    def __init__(self, routine, test_arg, post_shift=0):
+        bfo.__init__(self, routine)
+        self.test_arg = test_arg
+        self.post_shift = post_shift
+    
+    def do(self, shifter, argmap):
+        pre_shift = shifter.shift(argmap[self.test_arg])
+        body = bfo.do(self, shifter, argmap)
+        post_shift = shifter.shift(argmap[self.test_arg], self.post_shift)
+        return pre_shift + '[' + body + post_shift + ']'
+ 
+# for example:
+#  inc = bfo_primitive('+')
+#  dec = bfo_primitive('-')
+#  mov = bfo_loop(((inc, (1,)), (dec, (0,))), 0)
